4385b8db5cdb2d5dd36ccb6676796874154b74b5
Lenovo <Lenovo@DESKTOP-3T7CVEG>
2024-07-03 09:01:29 +0530

    Min and max

--- Program.cs
+++ Program.cs
@@ -6,6 +6,13 @@ Console.WriteLine("Hello, World!");
 int[] arr = { 1, 2, 4, 5, 6 };
 Search.LinearSearch(arr, 5);
 Search.SearchChar("Riyaz", 'z');
+bool searchInRange = Search.SearchInRange(arr, 4, 1, 3);
+Console.WriteLine(searchInRange);
+
+int[] minArr = { 3, 56, 78, 35, -4 };
+int minValue = Search.MinNumber(minArr);
+Console.WriteLine(minValue);
+
 public class Search
 {
     public static int LinearSearch(int[] arr, int target)
@@ -42,4 +49,56 @@ public class Search
 
         return false;
     }
+
+    public static bool SearchInRange(int[] arr, int target, int start, int end)
+    {
+        if (arr.Length == 0)
+        {
+            return false;
+        }
+        for (int i = start; i < end; i++)
+        {
+            if (arr[i] == target)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int MinNumber(int[] arr)
+    {
+        if (arr.Length == 0)
+        {
+            return -1;
+        }
+        int min = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (min > arr[i]) { 
+            min = arr[i];
+            }
+        }
+        return min;
+
+    }
+
+    public static int MaxNumber(int[] arr)
+    {
+        if (arr.Length == 0)
+        {
+            return -1;
+        }
+        int max = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (max < arr[i])
+            {
+                max = arr[i];
+            }
+        }
+        return max;
+
+    }
+
 }
